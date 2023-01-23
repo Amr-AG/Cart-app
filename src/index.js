88da2505fab76a29ae9d0bab15ca53ae9bef22c1
Amr-AG <amrgamal011@yahoo.com>
2023-01-23 17:29:02 +0200

    dep

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { store } from "../../cart-app/src/RTK/store";
+import { store } from "./RTK/store";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
